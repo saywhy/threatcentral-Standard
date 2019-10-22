a78050a610fb8b299646e147b8a3bf4c2e74c00d
yuechanglin <yuechanglin@163.com>
2019-10-22 17:24:53 +0800

    导航栏新加文件

---
+++ frontend/views/layouts/aside.php
@@ -0,0 +1,254 @@
+<!--侧边栏 ycl20191021-->
+
+<div id="asideApp" ng-controller="asideCtrl" ng-cloak>
+
+    <!--首页-->
+    <ul class="aside-lists" ng-show="indexCode === 0">
+        <li class="aside-item">
+            <a class="aside-item-a" ng-click="menu_aside.site.system = !menu_aside.site.system">
+                <i class="fa fa-home"></i>
+                <span class="name">IT 系统首页</span>
+                <span class="caret" ng-class="{'active':!menu_aside.site.system}"></span>
+            </a>
+            <ul class="aside-lists-second" ng-show="menu_aside.site.system">
+                <li class="aside-item-second <?=isActive(['/site/index', '/'])?>" ng-if="menu_list.index_overview">
+                    <a class="aside-item-second-a" href="<?=getPath('/site/index')?>">
+                        <span>概览</span>
+                    </a>
+                </li>
+                <li class="aside-item-second <?=isActive(['/map.html'])?>" ng-if="menu_list.index_BigScreen">
+                    <a class="aside-item-second-a" href="<?=getPath('/map.html')?>">
+                        <span>可视化大屏</span>
+                    </a>
+                </li>
+            </ul>
+        </li>
+
+        <li class="aside-item">
+             <a class="aside-item-a" href="javascript:void(0);">
+                <i class="fa fa-home"></i>
+                <span class="name">车联网首页</span>
+                <!--<span class="caret"></span>-->
+             </a>
+        </li>
+    </ul>
+
+    <!--情报-->
+    <ul class="aside-lists" ng-show="indexCode === 1">
+        <li class="aside-item">
+            <a class="aside-item-a" ng-click="menu_aside.search.system = !menu_aside.search.system">
+                <i class="fa fa-podcast"></i>
+                <span class="name">IT 系统情报</span>
+                <span class="caret" ng-class="{'active':!menu_aside.search.system}"></span>
+            </a>
+            <ul class="aside-lists-second" ng-show="menu_aside.search.system">
+                <li class="aside-item-second <?=isActive(['/search/index'])?>"
+                  ng-if="menu_list.intelligence_query">
+                  <a class="aside-item-second-a" href="<?=getPath('/search/index')?>" >
+                      <span>情报查询</span>
+                  </a>
+                </li>
+                <li class="aside-item-second <?=isActive(['/agent/index'])?>"
+                  ng-if="menu_list.intelligence_extract">
+                  <a class="aside-item-second-a" href="<?=getPath('/agent/index')?>" >
+                      <span>情报聚合</span>
+                  </a>
+                </li>
+                <li class="aside-item-second <?=isActive(['/share/index'])?>"
+                  ng-if="menu_list.intelligence_share">
+                  <a class="aside-item-second-a" href="<?=getPath('/share/index')?>">
+                      <span>情报共享</span>
+                  </a>
+                </li>
+                <li class="aside-item-second <?=isActive(['/intelligence/source-management'])?>"
+                  ng-if="menu_list.intelligence_sourceAdmin">
+                  <a class="aside-item-second-a" href="<?=getPath('/intelligence/source-management')?>" >
+                      <span>情报源管理</span>
+                  </a>
+                </li>
+                <li class="aside-item-second <?=isActive(['/search/apt-lib'])?>"
+                  ng-if="menu_list.intelligence_apt">
+                  <a class="aside-item-second-a" href="<?=getPath('/search/apt-lib')?>" >
+                      <span>APT武器库</span>
+                  </a>
+                </li>
+            </ul>
+        </li>
+
+        <li class="aside-item">
+             <a class="aside-item-a" href="javascript:void(0);">
+                <i class="fa fa-podcast"></i>
+                <span class="name">车联网情报</span>
+                <!--<span class="caret"></span>-->
+             </a>
+        </li>
+    </ul>
+
+    <!--资产-->
+    <ul class="aside-lists" ng-show="indexCode === 2">
+        <li class="aside-item">
+            <a class="aside-item-a" ng-click="menu_aside.assets.system = !menu_aside.assets.system">
+                <i class="fa fa-database"></i>
+                <span class="name">IT 系统资产</span>
+                <span class="caret" ng-class="{'active':!menu_aside.assets.system}"></span>
+            </a>
+            <ul class="aside-lists-second" ng-show="menu_aside.assets.system">
+               <li class="aside-item-second <?=isActive(['/assets/asset-management'])?>"
+                   ng-if="menu_list.assets_admin">
+                   <a class="aside-item-second-a" href="<?=getPath('/assets/asset-management')?>" >
+                       <span>资产管理</span>
+                   </a>
+               </li>
+               <li class="aside-item-second <?=isActive(['/assets/asset-risky'])?>"
+                   ng-if="menu_list.assets_risk">
+                   <a class="aside-item-second-a" href="<?=getPath('/assets/asset-risky')?>" >
+                       <span>受影响资产</span>
+                   </a>
+               </li>
+            </ul>
+        </li>
+
+        <li class="aside-item">
+             <a class="aside-item-a" href="javascript:void(0);">
+                <i class="fa fa-database"></i>
+                <span class="name">车联网资产</span>
+                <!--<span class="caret"></span>-->
+             </a>
+        </li>
+    </ul>
+
+    <!--预警-->
+    <ul class="aside-lists" ng-show="indexCode === 3">
+        <li class="aside-item">
+            <a class="aside-item-a" ng-click="menu_aside.alert.system = !menu_aside.alert.system">
+                <i class="fa fa-heartbeat"></i>
+                <span class="name">IT 系统预警</span>
+                <span class="caret" ng-class="{'active':!menu_aside.alert.system}"></span>
+            </a>
+            <ul class="aside-lists-second" ng-show="menu_aside.alert.system">
+               <li class="aside-item-second <?=isActive(['/alert/index'])?>"
+                   ng-if="menu_list.warning_threat">
+                   <a class="aside-item-second-a" href="<?=getPath('/alert/index')?>" >
+                       <span>威胁预警</span>
+                   </a>
+               </li>
+               <li class="aside-item-second <?=isActive(['/alert/loophole'])?>"
+                   ng-if="menu_list.warning_loophole">
+                   <a class="aside-item-second-a" href="<?=getPath('/alert/loophole')?>" >
+                       <span>漏洞预警</span>
+                   </a>
+               </li>
+               <li class="aside-item-second <?=isActive(['/alert/darknet'])?>"
+                   ng-if="menu_list.warning_drakNet">
+                   <a class="aside-item-second-a" href="<?=getPath('/alert/darknet')?>">
+                       <span>暗网预警</span>
+                   </a>
+               </li>
+            </ul>
+        </li>
+
+        <li class="aside-item">
+             <a class="aside-item-a" href="javascript:void(0);">
+                <i class="fa fa-heartbeat"></i>
+                <span class="name">车联网预警</span>
+                <!--<span class="caret"></span>-->
+             </a>
+        </li>
+    </ul>
+
+    <!--报表-->
+    <ul class="aside-lists" ng-show="indexCode === 4">
+        <li class="aside-item">
+            <a class="aside-item-a" ng-click="menu_aside.report.system = !menu_aside.report.system">
+                <i class="fa fa-area-chart"></i>
+                <span class="name">IT 系统报表</span>
+                <span class="caret" ng-class="{'active':!menu_aside.report.system}"></span>
+            </a>
+            <ul class="aside-lists-second" ng-show="menu_aside.report.system">
+               <li class="aside-item-second <?=isActive(['/report/index'])?>"
+                   ng-if="menu_list.report_creat">
+                   <a class="aside-item-second-a" href="<?=getPath('/report/index')?>">
+                       <span>报表生成</span>
+                   </a>
+               </li>
+               <li class="aside-item-second <?=isActive(['/report/send'])?>" ng-if="menu_list.report_send">
+                   <a class="aside-item-second-a" href="<?=getPath('/report/send')?>" >
+                       <span>报表发送</span>
+                   </a>
+               </li>
+            </ul>
+        </li>
+
+        <li class="aside-item">
+             <a class="aside-item-a" href="javascript:void(0);">
+                <i class="fa fa-area-chart"></i>
+                <span class="name">车联网报表</span>
+                <!--<span class="caret"></span>-->
+             </a>
+        </li>
+    </ul>
+
+    <!--设置-->
+    <ul class="aside-lists" ng-show="indexCode === 5">
+        <li class="aside-item">
+            <a class="aside-item-a" ng-click="menu_aside.seting.system = !menu_aside.seting.system">
+                <i class="fa fa-cog"></i>
+                <span class="name">IT 系统社设置</span>
+                <span class="caret" ng-class="{'active':!menu_aside.seting.system}"></span>
+            </a>
+            <ul class="aside-lists-second" ng-show="menu_aside.seting.system">
+               <li class="aside-item-second <?=isActive(['/seting/network'])?>" ng-if="menu_list.set_sys">
+                   <a class="aside-item-second-a" href="<?=getPath('/seting/network')?>" >
+                       <span>网络配置</span>
+                   </a>
+               </li>
+               <li class="aside-item-second <?=isActive(['/seting/systemnotice'])?>"
+                   ng-if="menu_list.set_notice">
+                   <a class="aside-item-second-a" href="<?=getPath('/seting/systemnotice')?>" >
+                       <span>威胁通知</span>
+                   </a>
+               </li>
+               <li class="aside-item-second <?=isActive(['/seting/custom-information-search'])?>"
+                   ng-if="menu_list.set_loopholeRelation">
+                   <a class="aside-item-second-a" href="<?=getPath('/seting/custom-information-search')?>">
+                       <span>漏洞关联</span>
+                   </a>
+               </li>
+               <li class="aside-item-second <?=isActive(['/seting/centralmanager'])?>"
+                   ng-if="menu_list.set_admin">
+                   <a class="aside-item-second-a" href="<?=getPath('/seting/centralmanager')?>">
+                       <span>集中管理</span>
+                   </a>
+               </li>
+               <li class="aside-item-second <?=isActive(['/seting/user'])?>" ng-if="menu_list.set_user">
+                   <a class="aside-item-second-a" href="<?=getPath('/seting/user')?>" >
+                       <span>账号管理</span>
+                   </a>
+               </li>
+               <li class="aside-item-second <?=isActive(['/seting/log'])?>" ng-if="menu_list.set_log">
+                   <a class="aside-item-second-a" href="<?=getPath('/seting/log')?>" >
+                       <span>审计日志</span>
+                   </a>
+               </li>
+               <li class="aside-item-second <?=isActive(['/api/index'])?>" ng-if="menu_list.api">
+                   <a class="aside-item-second-a" href="<?=getPath('/api/index')?>" >
+                       <span>情报API</span>
+                   </a>
+               </li>
+            </ul>
+        </li>
+
+        <li class="aside-item">
+             <a class="aside-item-a" href="javascript:void(0);">
+                <i class="fa fa-cog"></i>
+                <span class="name">车联网设置</span>
+                <!--<span class="caret"></span>-->
+             </a>
+        </li>
+    </ul>
+</div>
+
+<script type="text/javascript" src="/js/controllers/aside.js"></script>
+
+
+
---
+++ frontend/web/js/controllers/aside.js
@@ -0,0 +1,222 @@
+var myApp = angular.module("asideApp", []);
+
+myApp.controller("asideCtrl", function($scope,$http) {
+
+    $scope.indexCode = 0;
+    $scope.menu_aside = {
+        site: {
+            system: true,
+            vehicle: false
+        },
+        search: {
+            system: true,
+            vehicle: false
+        },
+        assets: {
+            system: true,
+            vehicle: false
+        },
+        alert: {
+            system: true,
+            vehicle: false
+        },
+        report: {
+            system: true,
+            vehicle: false
+        },
+        seting: {
+            system: true,
+            vehicle: false
+        }
+    };
+
+    $scope.menu_list = {
+        //   首页
+        index: false,
+        index_overview: false,
+        index_BigScreen: false,
+        // 情报
+        intelligence: false,
+        intelligence_query: false,
+        intelligence_extract: false,
+        intelligence_share: false,
+        intelligence_sourceAdmin: false,
+        intelligence_apt: false,
+        // 资产
+        assets: false,
+        assets_admin: false,
+        assets_risk: false,
+        // 预警
+        warning: false,
+        warning_threat: false,
+        warning_loophole: false,
+        warning_drakNet: false,
+        // 报表
+        report: false,
+        report_creat: false,
+        report_send: false,
+        // 设置
+        set: false,
+        set_sys: false,
+        set_notice: false,
+        set_loopholeRelation: false,
+        set_admin: false,
+        set_user: false,
+        set_log: false,
+        api: false
+    };
+
+    $scope.getProjectName = function(){
+        //获取主机地址之后的目录
+        var pathName = window.document.location.pathname;
+        var projectName = pathName.substring(1, pathName.substr(1).indexOf('/')+1);
+        return projectName;
+    }
+
+    $scope.init_code = function(){
+
+        let names = $scope.getProjectName();
+        if(names === 'site' || names == null || names== '/'){
+            $scope.indexCode = 0;
+        }else if(names === 'search' || names === 'agent' || names === 'share' || names === 'intelligence'){
+            $scope.indexCode = 1;
+        }else if(names === 'assets'){
+            $scope.indexCode = 2;
+        }else if(names === 'alert'){
+            $scope.indexCode = 3;
+        }else if(names === 'report'){
+            $scope.indexCode = 4;
+        }else if(names === 'seting' || names === 'api'){
+            $scope.indexCode = 5;
+        }
+    }
+
+    $scope.get_menu = function() {
+        $http.get("/site/menu").then(
+            function success(data) {
+                if (data.data.status == "success") {
+                    angular.forEach(data.data.data, function(item) {
+                        // 首页
+                        if (item.permissions_id == "1") {
+                            $scope.menu_list.index = true;
+                            angular.forEach(item.child_menu, function(child) {
+                                if (child.permissions_id == "2") {
+                                    $scope.menu_list.index_overview = true;
+                                }
+                                if (child.permissions_id == "14") {
+                                    $scope.menu_list.index_BigScreen = true;
+                                }
+                            });
+                        }
+                        // 情报
+                        if (item.permissions_id == "15") {
+                            $scope.menu_list.intelligence = true;
+                            angular.forEach(item.child_menu, function(child) {
+                                if (child.permissions_id == "16") {
+                                    $scope.menu_list.intelligence_query = true;
+                                }
+                                if (child.permissions_id == "24") {
+                                    $scope.menu_list.intelligence_extract = true;
+                                }
+                                if (child.permissions_id == "29") {
+                                    $scope.menu_list.intelligence_share = true;
+                                }
+                                if (child.permissions_id == "46") {
+                                    $scope.menu_list.intelligence_sourceAdmin = true;
+                                }
+                                if (child.permissions_id == "50") {
+                                    $scope.menu_list.intelligence_apt = true;
+                                }
+                            });
+                        }
+                        // 资产
+                        if (item.permissions_id == "54") {
+                            $scope.menu_list.assets = true;
+                            angular.forEach(item.child_menu, function(child) {
+                                if (child.permissions_id == "55") {
+                                    $scope.menu_list.assets_admin = true;
+                                }
+                                if (child.permissions_id == "72") {
+                                    $scope.menu_list.assets_risk = true;
+                                }
+                            });
+                        }
+                        // 预警
+                        if (item.permissions_id == "77") {
+                            $scope.menu_list.warning = true;
+                            angular.forEach(item.child_menu, function(child) {
+                                if (child.permissions_id == "78") {
+                                    $scope.menu_list.warning_threat = true;
+                                }
+                                if (child.permissions_id == "85") {
+                                    $scope.menu_list.warning_loophole = true;
+                                }
+                                if (child.permissions_id == "90") {
+                                    $scope.menu_list.warning_drakNet = true;
+                                }
+                            });
+                        }
+                        // 报表
+                        if (item.permissions_id == "127") {
+                            $scope.menu_list.report = true;
+                            angular.forEach(item.child_menu, function(child) {
+                                if (child.permissions_id == "128") {
+                                    $scope.menu_list.report_creat = true;
+                                }
+                                if (child.permissions_id == "129") {
+                                    $scope.menu_list.report_send = true;
+                                }
+                            });
+                        }
+                        // 设置
+                        if (item.permissions_id == "93") {
+                            $scope.menu_list.set = true;
+                            angular.forEach(item.child_menu, function(child) {
+                                if (child.permissions_id == "94") {
+                                    $scope.menu_list.set_sys = true;
+                                }
+                                if (child.permissions_id == "97") {
+                                    $scope.menu_list.set_notice = true;
+                                }
+                                if (child.permissions_id == "104") {
+                                    $scope.menu_list.set_loopholeRelation = true;
+                                }
+                                if (child.permissions_id == "130") {
+                                    $scope.menu_list.set_admin = true;
+                                }
+                                if (child.permissions_id == "110") {
+                                    $scope.menu_list.set_user = true;
+                                }
+                                if (child.permissions_id == "126") {
+                                    $scope.menu_list.set_log = true;
+                                }
+                                if (child.permissions_id == "151") {
+                                    $scope.menu_list.api = true;
+                                }
+                            });
+                        }
+                    });
+                }
+            },
+            function err(rsp) {}
+        );
+    };
+
+    $scope.init = function() {
+
+        //左侧栏显示判定
+        $scope.init_code();
+        //左侧栏权限设置
+        $scope.get_menu();
+
+
+    };
+
+    $scope.init();
+
+});
+
+
+angular.element(document).ready(function() {
+    angular.bootstrap(document.getElementById("asideApp"), ['asideApp']);
+});
