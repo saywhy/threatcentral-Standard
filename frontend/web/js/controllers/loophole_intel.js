0fe12c7e6181eebce2a7a52b08a09d71d5ce6c84
zhongshuaisun <zhongshuaisun@126.com>
2019-11-05 17:59:27 +0800

    设置漏洞情报管理 编辑未完

--- frontend/web/js/controllers/loophole_intel.js
+++ frontend/web/js/controllers/loophole_intel.js
@@ -1,34 +1,69 @@
 var myApp = angular.module("myApp", []);
 myApp.controller("loopholeIntelCtrl", function ($scope, $http, $filter) {
     $scope.init = function () {
-        $scope.get_page();
         $scope.searchTime = {
-            startDate: moment()
-                .subtract(90, "days")
-                .unix(),
-            endDate: moment().unix()
+            startDate: moment().subtract(90, "days"),
+            endDate: moment()
         };
         $scope.seach_data = {
-            source: '所有',
-            stauts: 0,
+            source: '全部',
+            stauts: '',
+            label_id: '',
+            key_word: '',
+            level: '',
+            startDate: moment().subtract(90, "days").unix(),
+            endDate: moment().unix(),
         };
         $scope.status_search = [{
-                num: 0,
-                status: '所有'
+                num: '',
+                status: '全部'
             },
             {
-                num: 1,
+                num: '1',
                 status: '已发布'
             },
             {
-                num: 2,
+                num: '0',
                 status: '未发布'
             }
         ]
+        $scope.search_level = [{
+                num: '',
+                status: '全部'
+            },
+            {
+                num: '高',
+                status: '高'
+            },
+            {
+                num: '中',
+                status: '中'
+            },
+            {
+                num: '低',
+                status: '低'
+            }
+        ]
+        $scope.add_level = [{
+                num: '高',
+                status: '高'
+            },
+            {
+                num: '中',
+                status: '中'
+            },
+            {
+                num: '低',
+                status: '低'
+            }
+        ]
+
+        $scope.add_startDate = moment().unix();
         $scope.picker_search();
         $scope.start_time_picker();
         $scope.get_loophole_source();
         $scope.get_tag_list();
+        $scope.get_page();
         $scope.tag_list_if = false;
     }
     // 初始化时间
@@ -48,8 +83,7 @@ myApp.controller("loopholeIntelCtrl", function ($scope, $http, $filter) {
                 }
             },
             function (start, end, label) {
-                console.log(start.unix());
-                console.log(end.unix());
+                $scope.add_startDate = start.unix()
             }
         );
     };
@@ -70,9 +104,8 @@ myApp.controller("loopholeIntelCtrl", function ($scope, $http, $filter) {
                 }
             },
             function (start, end, label) {
-
-                console.log(start.unix());
-                console.log(end.unix());
+                $scope.seach_data.startDate = start.unix();
+                $scope.seach_data.endDate = endDate.unix();
             }
         );
     };
@@ -92,28 +125,54 @@ myApp.controller("loopholeIntelCtrl", function ($scope, $http, $filter) {
                     $scope.loop_source.push(item.sourse);
                     $scope.loop_source_add.push(item.sourse);
                 })
-                $scope.loop_source.push('所有');
+                $scope.loop_source.push('全部');
                 $scope.loop_source_add.push('请选择');
             },
             function () {}
         );
     }
-
-    $scope.get_page = function () {
-        var data = {
-            stime: '',
-            etime: '',
-            sourse: '',
-            status: '',
-            level: '',
-            detail: '',
+    // 获取列表
+    $scope.get_page = function (pageNow) {
+        pageNow = pageNow ? pageNow : 1;
+        var loading = zeroModal.loading(4);
+        var params_data = {
+            source: '',
+            label_id: [],
+            label_id_box: [],
+            label_id_str: '',
         }
-        $http.get("/seting/loophole-intelligence-list", data).then(
-            function success(data) {
-                console.log(data);
+        if ($scope.seach_data.source != '全部') {
+            console.log('1231');
+            params_data.source = $scope.seach_data.source
+        }
+        if ($scope.seach_data.label_id != '') {
+            params_data.label_id.push($scope.seach_data.label_id * 1);
+            params_data.label_id_box.push(params_data.label_id)
+            params_data.label_id_str = JSON.stringify(params_data.label_id_box);
+        } else {
+            params_data.label_id_str = '[]'
+        }
+        console.log(params_data);
+        $http({
+            method: "get",
+            url: "/vehicleintelligence/loophole-intelligence-list",
+            params: {
+                stime: $scope.seach_data.startDate,
+                etime: $scope.seach_data.endDate,
+                sourse: params_data.source,
+                status: $scope.seach_data.stauts,
+                level: $scope.seach_data.level,
+                label_id: params_data.label_id_str,
+                key_word: $scope.seach_data.key_word,
+                page: pageNow,
+                rows: 10,
+            }
+        }).then(
+            function (data) {
+                zeroModal.close(loading);
                 $scope.pages = data.data;
             },
-            function err(data) {}
+            function () {}
         );
     }
     $scope.add_loophole_inteligence = function () {
@@ -143,7 +202,7 @@ myApp.controller("loopholeIntelCtrl", function ($scope, $http, $filter) {
     $scope.add_loop_box = function (item) {
         $scope.alert_item = {
             title: '',
-            level: '',
+            level: '高',
             first_seen_time: '',
             sourse: '请选择',
             detail: '',
@@ -152,10 +211,11 @@ myApp.controller("loopholeIntelCtrl", function ($scope, $http, $filter) {
                 unexist: []
             },
             tag_list: [],
-            tag_list_str: ''
+            add_new_tag: [],
+            tag_list_str: '',
         }
         var W = 828;
-        var H = 532;
+        var H = 600;
         zeroModal.show({
             title: "情报录入",
             content: alert_time,
@@ -170,41 +230,160 @@ myApp.controller("loopholeIntelCtrl", function ($scope, $http, $filter) {
         });
     };
     //   取消弹窗
-    $scope.alert_time_cancel = function () {
+    $scope.add_cancel = function () {
         zeroModal.closeAll();
     };
-    $scope.alert_sure = function () {
-
+    // 添加漏洞情报
+    $scope.add_sure = function () {
+        var loading = zeroModal.loading(4);
+        var label_id_exist = []
+        if ($scope.alert_item.label_id.exist.length != 0) {
+            angular.forEach($scope.alert_item.label_id.exist, function (item) {
+                label_id_exist.push(item.id)
+            })
+        }
+        if ($scope.alert_item.sourse == '请选择') {
+            return false
+        }
+        $http({
+            method: "post",
+            url: "/seting/loophole-intelligence-add",
+            data: {
+                title: $scope.alert_item.title,
+                level: $scope.alert_item.level,
+                first_seen_time: $scope.add_startDate,
+                sourse: $scope.alert_item.sourse,
+                detail: $scope.alert_item.detail,
+                label_id: {
+                    exist: label_id_exist,
+                    unexist: $scope.alert_item.add_new_tag
+                }
+            }
+        }).then(
+            function (data) {
+                zeroModal.close(loading);
+                if (data.data.status == 'success') {
+                    zeroModal.success("添加成功");
+                }
+                setTimeout(zeroModal.closeAll(), 3000)
+                $scope.get_page();
+            },
+            function () {}
+        );
     };
     // 触发标签选择
     $scope.tag_focus = function () {
         $scope.tag_list_if = true;
+        $scope.get_tag_list($scope.alert_item.tag_list_str);
     }
     $scope.tag_blur = function () {
         if ($scope.alert_item.tag_list_str == '') {
             return false;
         }
-        $scope.alert_item.tag_list.push($scope.alert_item.tag_list_str);
         $scope.alert_item.tag_list_str = '';
     }
     // 获取标签列表
-    $scope.get_tag_list = function () {
-        var data = {
-            label_name: ''
-        }
-        $http.get("/site/get-label", data).then(
-            function success(data) {
+    $scope.get_tag_list = function (name) {
+        $http({
+            method: "get",
+            url: "/site/get-label",
+            params: {
+                label_name: name,
+            }
+        }).then(
+            function (data) {
                 console.log(data);
+                $scope.search_tag_list = [];
                 $scope.tag_list = data.data;
+                $scope.search_tag_list_str = JSON.stringify(data.data)
+                $scope.search_tag_list = JSON.parse($scope.search_tag_list_str);
+                $scope.search_tag_list.push({
+                    id: '',
+                    label_name: '全部'
+                })
             },
-            function err(data) {}
+            function () {}
         );
+    }
+    $scope.tag_change = function (name) {
+        $scope.get_tag_list(name);
     }
     // 选择标签
     $scope.tag_list_item = function (item) {
-        $scope.alert_item.tag_list.push(item);
+        $scope.alert_item.tag_list.push(item.label_name);
+        $scope.alert_item.label_id.exist.push(item);
         $scope.tag_list_if = false;
     }
+    // 删除标签
+    $scope.tag_del = function (name, index) {
+        angular.forEach($scope.alert_item.add_new_tag, function (value, key) {
+            if ($scope.alert_item.tag_list[index] == value) {
+                $scope.alert_item.add_new_tag.splice(key, 1);
+            }
+        })
+        angular.forEach($scope.alert_item.label_id.exist, function (item, key) {
+            if ($scope.alert_item.tag_list[index] == item.label_name) {
+                $scope.alert_item.label_id.exist.splice(key, 1);
+            }
+        })
+        $scope.alert_item.tag_list.splice(index, 1);
+    }
+    $scope.mykey = function (e) {
+        var keycode = window.event ? e.keyCode : e.which; //获取按键编码
+        if (keycode == 13) {
+            if ($scope.alert_item.tag_list_str == '') {
+                $scope.tag_list_if = false;
+                setTimeout("$('.tag_input')[0].blur()", 500);
+                return false;
+            }
+            $scope.alert_item.tag_list.push($scope.alert_item.tag_list_str);
+            $scope.alert_item.add_new_tag.push($scope.alert_item.tag_list_str);
+            $scope.alert_item.tag_list_str = '';
+            $scope.tag_list_if = false;
+            setTimeout("$('.tag_input')[0].blur()", 500);
+        }
+    }
+    // 发布漏洞情报
+    $scope.release = function (id) {
+        var loading = zeroModal.loading(4);
+        $http({
+            method: "put",
+            url: "/seting/loophole-intelligence-publish",
+            data: {
+                id: id
+            }
+        }).then(
+            function (data) {
+                zeroModal.close(loading);
+                if (data.data.status == 'success') {
+                    zeroModal.success("发布成功");
+                }
+                $scope.get_page();
+            },
+            function () {}
+        );
+    }
+    // 删除漏洞情报
+    $scope.delete = function (id) {
+        var loading = zeroModal.loading(4);
+        $http({
+            method: "delete",
+            url: "/seting/special-intelligence-del",
+            data: {
+                id: id
+            }
+        }).then(
+            function (data) {
+                zeroModal.close(loading);
+                if (data.data.status == 'success') {
+                    zeroModal.success("删除成功");
+                }
+                $scope.get_page();
+            },
+            function () {}
+        );
+    }
+
 
     $scope.init();
 
