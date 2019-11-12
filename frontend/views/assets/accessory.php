6390fc7e7b4c96bc10382b2e16c0af67027c0f0e
yuechanglin <yuechanglin@163.com>
2019-11-12 12:10:50 +0800

    no message

--- frontend/views/assets/accessory.php
+++ frontend/views/assets/accessory.php
@@ -2,12 +2,48 @@
 /* @var $this yii\web\View */
 $this->title = '零配件资产';
 ?>
-<style>
-
-</style>
+<link rel="stylesheet" href="/css/assets/accessory.css">
 <section class="assets_accessory_container" ng-app="myApp" ng-controller="assetsAccessoryCtrl" ng-cloak>
-    <div class="log_box">
-         零配件资产
+    <div class="assets_special">
+        <div class="assets_box_top">
+
+            <!-- PNID -->
+            <select class="accessory_search_input source_input" ng-model="acc_data.id"
+              ng-options="x.num as x.status for x in accssory.PN_ID">
+            </select>
+
+            <!-- PN名称 -->
+            <select class="accessory_search_input source_input" ng-model="acc_data.name"
+              ng-options="x.num as x.status for x in accssory.PN_NAME">
+            </select>
+
+            <!-- 供应商 -->
+            <select class="accessory_search_input source_input" ng-model="acc_data.sup"
+              ng-options="x.num as x.status for x in accssory.PN_SUP">
+            </select>
+
+            <!-- 操作系统 -->
+            <select class="accessory_search_input source_input" ng-model="acc_data.sys"
+              ng-options="x.num as x.status for x in accssory.PN_SYS">
+            </select>
+
+            <!-- 操作系统内核 -->
+            <select class="accessory_search_input source_input" ng-model="acc_data.core"
+              ng-options="x.num as x.status for x in accssory.PN_CORE">
+            </select>
+
+            <!-- 供应商ID -->
+            <select class="accessory_search_input source_input" ng-model="acc_data.sup_id"
+              ng-options="x.num as x.status for x in accssory.PN_SUP_ID">
+            </select>
+
+            <!-- 筛选 -->
+            <button class="button_search" ng-click="get_page()" ng-keyup="label_keyup($event)">搜索</button>
+
+            <!-- 重置 -->
+            <button class="button_search" ng-click="get_page()" ng-keyup="label_keyup($event)">搜索</button>
+       </div>
+
     </div>
 </section>
 <script src="/js/controllers/assets_accessory.js"></script>
--- frontend/web/js/controllers/assets_accessory.js
+++ frontend/web/js/controllers/assets_accessory.js
@@ -1,4 +1,27 @@
 var myApp = angular.module("myApp", []);
 myApp.controller("assetsAccessoryCtrl", function($scope, $http, $filter) {
 
+    $scope.init = function () {
+
+        $scope.acc_data = {
+            id: '',
+            name: '',
+            sup: '',
+            sys: '',
+            core: '',
+            sup_id: ''
+        };
+
+        $scope.accssory = {
+            PN_ID:[{num: '', status: '全部'}, {num: '高', status: '高'}, {num: '中', status: '中'}, {num: '低', status: '低'}],
+            PN_NAME:[{num: '', status: '全部'}, {num: '高', status: '高'}, {num: '中', status: '中'}, {num: '低', status: '低'}],
+            PN_SUP:[{num: '', status: '全部'}, {num: '高', status: '高'}, {num: '中', status: '中'}, {num: '低', status: '低'}],
+            PN_SYS:[{num: '', status: '全部'}, {num: '高', status: '高'}, {num: '中', status: '中'}, {num: '低', status: '低'}],
+            PN_CORE:[{num: '', status: '全部'}, {num: '高', status: '高'}, {num: '中', status: '中'}, {num: '低', status: '低'}],
+            PN_SUP_ID:[{num: '', status: '全部'}, {num: '高', status: '高'}, {num: '中', status: '中'}, {num: '低', status: '低'}]
+        }
+    }
+
+
+    $scope.init();
 });
