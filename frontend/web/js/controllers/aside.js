e2bb4488023f76293a74ae815d2b3cfb601f14ca
yuechanglin <yuechanglin@163.com>
2020-03-30 17:33:34 +0800

    侧边栏全部展开

--- frontend/web/js/controllers/aside.js
+++ frontend/web/js/controllers/aside.js
@@ -6,30 +6,30 @@ myApp.controller("asideCtrl", function($scope,$http,$timeout) {
     $scope.alertDetail = false;
     $scope.menu_aside = {
         site: {
-            system: false,
-            vehicle: false
+            system: true,
+            vehicle: true
         },
         search: {
-            system: false,
-            vehicle: false
+            system: true,
+            vehicle: true
         },
         assets: {
-            system: false,
-            vehicle: false
+            system: true,
+            vehicle: true
         },
         alert: {
-            system: false,
-            vehicle: false
+            system: true,
+            vehicle: true
         },
         report: {
-            system: false,
-            vehicle: false
+            system: true,
+            vehicle: true
         },
         seting: {
-            system: false,
-            vehicle: false,
+            system: true,
+            vehicle: true,
             manage:{
-                set: false
+                set: true
             }
         }
     };
@@ -175,169 +175,6 @@ myApp.controller("asideCtrl", function($scope,$http,$timeout) {
         }
 
     }
-
-    // 接口调用
-   /* $scope.getPermiss = function () {
-        return $http.get('/site/menu');
-    }
-
-    $scope.get_menu = async function() {
-
-        let str = window.localStorage.permission;
-
-        var permission = '';
-
-        if(str != undefined){
-            permission = JSON.parse(str);
-        }else {
-            permission = await $scope.getPermiss().then(function (resp) {
-
-                let locale = window.localStorage;
-                let permission = JSON.stringify(resp.data);
-                locale.permission = permission;
-
-                return resp.data;
-            });
-        }
-
-        $timeout(function () {
-            if (permission.status == "success") {
-
-                angular.forEach(permission.data, function(item) {
-                    // 首页
-                    if (item.permissions_id == "1") {
-                        //$scope.menu_list.index = true;
-                        angular.forEach(item.child_menu, function(child) {
-                            if (child.permissions_id == "2") {
-                                $scope.menu_list.index_overview = true;
-                            }
-                            if (child.permissions_id == "14") {
-                                $scope.menu_list.index_BigScreen = true;
-                            }
-                        });
-                    }
-                    // 情报
-                    if (item.permissions_id == "15") {
-                        //$scope.menu_list.intelligence = true;
-                        angular.forEach(item.child_menu, function(child) {
-                            if (child.permissions_id == "16") {
-                                $scope.menu_list.intelligence_query = true;
-                            }
-                            if (child.permissions_id == "24") {
-                                $scope.menu_list.intelligence_extract = true;
-                            }
-                            if (child.permissions_id == "29") {
-                                $scope.menu_list.intelligence_share = true;
-                            }
-                            if (child.permissions_id == "46") {
-                                $scope.menu_list.intelligence_sourceAdmin = true;
-                            }
-                            if (child.permissions_id == "50") {
-                                $scope.menu_list.intelligence_apt = true;
-                            }
-                            if (child.permissions_id == "186") {
-                                $scope.menu_list.intelligence_special = true;
-                            }
-                            if (child.permissions_id == "187") {
-                                $scope.menu_list.intelligence_loophole = true;
-                            }
-                        });
-                    }
-                    // 资产
-                    if (item.permissions_id == "54") {
-                        //$scope.menu_list.assets = true;
-                        angular.forEach(item.child_menu, function(child) {
-                            if (child.permissions_id == "55") {
-                                $scope.menu_list.assets_admin = true;
-                            }
-                            if (child.permissions_id == "72") {
-                                $scope.menu_list.assets_risk = true;
-                            }
-
-                            if (child.permissions_id == "184") {
-                                $scope.menu_list.assets_vehicle = true;
-                            }
-                            if (child.permissions_id == "185") {
-                                $scope.menu_list.assets_accessory = true;
-                            }
-                        });
-                    }
-                    // 预警
-                    if (item.permissions_id == "77") {
-                        //$scope.menu_list.warning = true;
-                        angular.forEach(item.child_menu, function(child) {
-                            if (child.permissions_id == "78") {
-                                $scope.menu_list.warning_threat = true;
-                            }
-                            if (child.permissions_id == "85") {
-                                $scope.menu_list.warning_loophole = true;
-                            }
-                            if (child.permissions_id == "90") {
-                                $scope.menu_list.warning_drakNet = true;
-                            }
-                            if (child.permissions_id == "205") {
-                                $scope.menu_list.warning_vehicle = true;
-                            }
-                        });
-                    }
-                    // 报表
-                    if (item.permissions_id == "127") {
-                        //$scope.menu_list.report = true;
-                        angular.forEach(item.child_menu, function(child) {
-                            if (child.permissions_id == "128") {
-                                $scope.menu_list.report_creat = true;
-                            }
-                            if (child.permissions_id == "129") {
-                                $scope.menu_list.report_send = true;
-                            }
-                        });
-                    }
-                    // 设置
-                    if (item.permissions_id == "93") {
-                        //$scope.menu_list.set = true;
-                        angular.forEach(item.child_menu, function(child) {
-                            if (child.permissions_id == "94") {
-                                $scope.menu_list.set_sys = true;
-                            }
-                            if (child.permissions_id == "97") {
-                                $scope.menu_list.set_notice = true;
-                            }
-                            if (child.permissions_id == "104") {
-                                $scope.menu_list.set_loopholeRelation = true;
-                            }
-                            if (child.permissions_id == "130") {
-                                $scope.menu_list.set_admin = true;
-                            }
-                            if (child.permissions_id == "110") {
-                                $scope.menu_list.set_user = true;
-                            }
-                            if (child.permissions_id == "126") {
-                                $scope.menu_list.set_log = true;
-                            }
-                            if (child.permissions_id == "151") {
-                                $scope.menu_list.api = true;
-                            }
-
-                            if (child.permissions_id == "181") {
-                                $scope.menu_list.set_label = true;
-                            }
-                            if (child.permissions_id == "182") {
-                                $scope.menu_list.set_special = true;
-                            }
-                            if (child.permissions_id == "183") {
-                                $scope.menu_list.set_loophole = true;
-                            }
-                            if (child.permissions_id == "209") {
-                                $scope.menu_list.set_base = true;
-                            }
-                        });
-                    }
-                });
-            }
-        },100)
-
-    };*/
-
 
     $scope.get_menu = function() {
 
@@ -491,7 +328,7 @@ myApp.controller("asideCtrl", function($scope,$http,$timeout) {
         //左侧栏权限设置
         $scope.get_menu();
         //左侧栏三角状态
-        $scope.get_status();
+       // $scope.get_status();
 
     };
 
